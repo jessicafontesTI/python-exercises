705c24bd64baa0fbad8dca2ea75114b9df7e80a5
jessicafontesTI <jessicafontesprofissional@outlook.com>
2026-07-21 23:26:24 -0300

    Implement arithmetic mean calculation in exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+#Média aritmética
+
+n1 = float(input('Digite a nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+media = (n1+n2) / 2
+print(f'A média entre {n1} e {n2} é igual a {media}')
