0fdb6e0c59a0bdda94142b6341658f8179f0e078
jessicafontesTI <jessicafontesprofissional@outlook.com>
2026-07-21 23:32:04 -0300

    Add wall painting area calculation script

---
+++ exercicio6.py
@@ -0,0 +1,7 @@
+#Pintar Parede
+
+a = float(input('Digite a altura da parede: '))
+l = float(input('Digite a largura da parede: '))
+r = a * l
+t = r / 2
+print(f'Sua parede tem dimensão de {a:.2f} x {l:.2f} e sua área é {r}m²\n Para pintar essa parede você precisará de {t:.2f} Litros de tinta.') 
