643e5f8dad4771d3039c322249aa2f7a605ef991
jessicafontesTI <jessicafontesprofissional@outlook.com>
2026-07-21 23:24:19 -0300

    Add calculations for double, triple, and square root

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+#DOBRO, TRIPLO, RAIZ QUADRADA
+
+num = int (input('Digite um número: '))
+dobro = num+num
+triplo = num*3
+raiz = num**(1/2)
+print(f'O Dobro de {num} vale: {dobro}\n O triplo de {num} vale: {triplo}\n A raiz quadrada de {num} vale: {raiz: .2f}')
